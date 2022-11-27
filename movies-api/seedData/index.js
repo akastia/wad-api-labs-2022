5d83b66a96a0ed5b9516c166caf29a4c3789e758
Mary Akastia Christo <n.akastia@gmail.com>
2022-11-27 22:57:29 +0000

    Topic 10 - Update movies data/route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -3,6 +3,8 @@ import users from './users';
 import dotenv from 'dotenv';
 import genres from './genres'
 import genreModel from '../api/genres/genreModel'
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 
 dotenv.config();
@@ -19,10 +21,24 @@ async function loadUsers() {
   }
 }
 
-if (process.env.SEED_DB) {
-  loadUsers();
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
 }
 
+if (process.env.SEED_DB) {
+  loadUsers();
+  loadGenres();//you may not need this line if you skipped the exercises
+  loadMovies();//ADD THIS LINE
+}
 
 // deletes all genres documents in collection and inserts test data
 async function loadGenres() {
