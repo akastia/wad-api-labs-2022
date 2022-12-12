c45359733e4b24d97cdd5e30a9981f87b354728b
Mary Akastia Christo <n.akastia@gmail.com>
2022-12-12 17:52:24 +0000

    Because of the revert some js were deleted, so added them back. The login doesn't work anymore

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -13,3 +13,17 @@ export const getUpcomingMovies = () => {
             throw error
         });
 };
+
+export const getNowPlayingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
+  };
